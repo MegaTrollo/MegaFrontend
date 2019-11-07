7723b97063973780755d31e8b9e0fd2b03dc280f
rwitczyk <Robert123>
2019-11-07 01:21:24 +0100

    Dodano mozliwosc tworzenia tablic, zmiany w navbarze i inne poprawki.

--- src/app/components/list-of-boards/list-of-boards.component.ts
+++ src/app/components/list-of-boards/list-of-boards.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Board} from '../../models/Board';
+import {ListofBoardsService} from '../../services/listof-boards.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-list-of-boards',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-of-boards.component.css']
 })
 export class ListOfBoardsComponent implements OnInit {
+  allBoards: Board[];
+  newBoard: Board;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private listofBoardsService: ListofBoardsService, private router: Router) {
   }
 
+  ngOnInit() {
+    this.listofBoardsService.getAllBoards().subscribe(value => {
+      this.allBoards = value;
+    }, error1 => {
+      console.log(error1.error.message);
+    });
+  }
+
+  createTable(boardName: string) {
+    console.log(boardName);
+    this.newBoard = new Board();
+    this.newBoard.name = boardName;
+    this.listofBoardsService.createNewBoard(this.newBoard).subscribe(value => {
+      // this.router.navigate(['list-of-boards']); // TODO: DO ZMIANY NA NOWY KOMPONENT TABLICY
+    }, error1 => {
+      console.log(error1.error.message);
+    });
+  }
 }
---
+++ src/app/services/listof-boards.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Board} from '../models/Board';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListofBoardsService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllBoards() {
+    return this.http.get<Board[]>(`${environment.localhostBackendUrl}/api/table/all`);
+  }
+
+  createNewBoard(newBoard: Board) {
+    return this.http.post(`${environment.localhostBackendUrl}/api/table/add`, newBoard);
+  }
+
+}
